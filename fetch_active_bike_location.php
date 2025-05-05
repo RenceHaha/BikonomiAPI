72227809194401007c0bf048f4b157bc686786a8
RnceHaha <marfil.markclarence.calisin@gmail.com>
2025-05-05 08:09:03 +0800

    minor tweaks active rental location

--- fetch_active_bike_location.php
+++ fetch_active_bike_location.php
@@ -16,7 +16,13 @@ if(!isset($data['account_id'])){
     exit;
 }
 
-$query = "SELECT longitude, latitude, bike_tbl.bike_id, bike_type_name, bike_name, rent_id, start_time, expected_end_time FROM bike_location
+$query = "SELECT longitude, latitude, bike_tbl.bike_id, bike_type_name, bike_name, rent_id, start_time, expected_end_time,
+CASE 
+    WHEN rental_tbl.end_time IS NULL 
+        AND TIMESTAMPDIFF(SECOND, rental_tbl.start_time, NOW()) > TIME_TO_SEC(rental_tbl.time_limit) THEN 'Overdue'
+    WHEN rental_tbl.end_time IS NULL THEN 'On Going'
+END AS rental_status
+FROM bike_location
 JOIN bike_tbl ON bike_location.bike_serial_gps = bike_tbl.bike_serial_gps
 JOIN bike_type_tbl ON bike_tbl.bike_type_id = bike_type_tbl.bike_type_id
 JOIN rental_tbl ON rental_tbl.bike_id = bike_tbl.bike_id
@@ -26,15 +32,15 @@ $stmt->bind_param("i", $data['account_id']);
 $stmt->execute();
 $result = $stmt->get_result();
 
-$locations = [];
+$bike_locations = [];
 while ($row = $result->fetch_assoc()) {
-    $locations[] = $row;
+    $bike_locations[] = $row;
 }
 if(empty($locations)){
     echo json_encode(['success' => false, 'message' => 'No Bike with GPS Active Rental Found']);
     exit; 
 }
-echo json_encode(['success' => true, 'locations' => $locations]);
+echo json_encode(['success' => true, 'bike_locations' => $bike_locations]);
 exit;
 
 ?>
